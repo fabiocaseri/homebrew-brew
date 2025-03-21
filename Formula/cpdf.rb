ba3294097aec92c959b99cc0ed61e89660451f53
Fabio Caseri <fabio.caseri@gmail.com>
2025-03-21 16:50:05 +0100

    cpdf: Add multiple architectures support
    Includes update to 2.8 version

--- Formula/cpdf.rb
+++ Formula/cpdf.rb
@@ -1,9 +1,35 @@
 class Cpdf < Formula
   desc "Coherent PDF Command-line Tools to modify PDF files"
-  homepage "https://www.coherentpdf.com/"
-  url "https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/d4e7ae79f65ef3f085d09e3c876eb9a8ed1ced34/OSX-Intel/cpdf"
-  version "2.4"
-  sha256 "8ddd3aac989cca5bdf9734dea233ba026a029d82b0e4fa745c7115de3519ffd8"
+  homepage "https://community.coherentpdf.com/"
+  version "2.8"
+  license "AGPL-3.0-only"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/06ce0022c741621e8b6aabba15e3f164a8b4daa6/OSX-Intel/cpdf"
+      sha256 "6f43a141447a71a5205eac180ad739c10f394638d311912fef73f95381673812"
+    end
+    if Hardware::CPU.arm?
+      url "https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/06ce0022c741621e8b6aabba15e3f164a8b4daa6/OSX-ARM/cpdf"
+      sha256 "d4c28bc920da89f8b7ddf40732cdeaba5bbe677ea3f8be1a9836c3a2dfe8254f"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel? 
+      if Hardware::CPU.is_64_bit?
+        url "https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/06ce0022c741621e8b6aabba15e3f164a8b4daa6/Linux-Intel-64bit/cpdf"
+        sha256 "efca6db7f6293021d653bc0d0ca20e11970701497bf70acdab3a3bac5d57b28f"
+      else
+        url "https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/06ce0022c741621e8b6aabba15e3f164a8b4daa6/Linux-Intel-32bit/cpdf"
+        sha256 "1cb206f50e6f911e931baf4f8c0ada0b36fecca5199739af2c981c3ab9a96f2f"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/06ce0022c741621e8b6aabba15e3f164a8b4daa6/Linux-ARM-64bit/cpdf"
+      sha256 "4003a5333fed2007540cf1f30bb748e67d8c813ed949ec597816a3b350562ea2"
+    end
+  end
 
   def install
     bin.install "cpdf"
